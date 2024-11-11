29df9fe050971752bcbac796584ac793b1df9317
TeKrop <tekrop@gmail.com>
2024-11-11 22:26:47 +0100

    finished day05. done day06

--- days/day05/main.py
+++ days/day05/main.py
@@ -1,4 +1,5 @@
 from scripts.utils import AbstractPuzzleSolver
+from itertools import pairwise
 
 
 class PuzzleSolver(AbstractPuzzleSolver):
@@ -18,14 +19,14 @@ class PuzzleSolver(AbstractPuzzleSolver):
             and not self.__contains_at_least_one_wrong_string(line)
         )
 
-    def __contains_at_least_three_vowels(self, line: str):
+    def __contains_at_least_three_vowels(self, line: str) -> bool:
         return len([char for char in line if char in self.vowels]) >= 3
 
     @staticmethod
-    def __contains_double_letters(line: str):
+    def __contains_double_letters(line: str) -> bool:
         return any(line[i] == line[i - 1] for i in range(1, len(line)))
 
-    def __contains_at_least_one_wrong_string(self, line: str):
+    def __contains_at_least_one_wrong_string(self, line: str) -> bool:
         return any(wrong_string in line for wrong_string in self.wrong_strings)
 
     ###########################
@@ -42,8 +43,23 @@ class PuzzleSolver(AbstractPuzzleSolver):
             line
         ) and self.__contains_letter_repeating_itself_with_one_between(line)
 
-    def __contains_pair_of_double_letters(self, line: str):
-        return
+    def __contains_pair_of_double_letters(self, line: str) -> bool:
+        letters_pairs = pairwise(line)
+        pair_positions: dict[str, int] = {}
 
-    def __contains_letter_repeating_itself_with_one_between(self, line: str):
-        return
+        # Loop over pairs to find out duplicates. Memorise every pair
+        # position so we can make sure we won't find overlapping pairs
+        for line_idx, letter_pair in enumerate(letters_pairs):
+            if letter_pair not in pair_positions:
+                pair_positions[letter_pair] = line_idx
+                continue
+
+            # Make sure the pairs aren't overlapping
+            # Else continue to search
+            if line_idx - pair_positions[letter_pair] > 1:
+                return True
+
+        return False
+
+    def __contains_letter_repeating_itself_with_one_between(self, line: str) -> bool:
+        return any(line[i - 1] == line[i + 1] for i in range(1, len(line) - 1))
--- days/day06/main.py
+++ days/day06/main.py
@@ -1,17 +1,105 @@
 from scripts.utils import AbstractPuzzleSolver
+from enum import StrEnum
 
 
 class PuzzleSolver(AbstractPuzzleSolver):
+    ###########################
+    # DAY 06 - Common Part
+    ###########################
+    lights_grid: "LightsGrid"
+    grid_size: int = 1_000
+
+    def _solve(self, brightness: bool = False) -> None:
+        self.lights_grid = LightsGrid(size=self.grid_size, brightness=brightness)
+        for line in self.lines:
+            self.lights_grid.apply_instruction_line(InstructionLine(line))
+        return self.lights_grid.lights_on
+
     ###########################
     # DAY 06 - First Part
     ###########################
 
     def _solve_first_part(self) -> int:
-        return None
+        return self._solve(brightness=False)
 
     ###########################
     # DAY 06 - Second Part
     ###########################
 
     def _solve_second_part(self) -> int:
-        return None
+        return self._solve(brightness=True)
+
+
+class Instruction(StrEnum):
+    TURN_ON = "turn on"
+    TOGGLE = "toggle"
+    TURN_OFF = "turn off"
+
+
+class InstructionLine:
+    instruction: Instruction
+    start_pos: tuple[int, int]
+    end_pos: tuple[int, int]
+
+    def __init__(self, line: str):
+        line_parts = line.split()
+        if len(line_parts) == 5:
+            instruction_start, instruction_end, start_pos, _, end_pos = line_parts
+            instruction = f"{instruction_start} {instruction_end}"
+        else:
+            instruction, start_pos, _, end_pos = line_parts
+
+        self.instruction = Instruction(instruction)
+        self.start_pos = tuple(int(value) for value in start_pos.split(","))
+        self.end_pos = tuple(int(value) for value in end_pos.split(","))
+
+    def __repr__(self) -> str:
+        return (
+            f"InstructionLine({self.instruction} -> {self.start_pos}..{self.end_pos})"
+        )
+
+    def apply(self, source_value: int, brightness: bool) -> int:
+        if not brightness:
+            return self._apply_on_off(source_value)
+        else:
+            return self._apply_brightness(source_value)
+
+    def _apply_on_off(self, source_value: int) -> int:
+        match self.instruction:
+            case Instruction.TURN_ON:
+                return 1
+            case Instruction.TURN_OFF:
+                return 0
+            case Instruction.TOGGLE:
+                return 1 if source_value == 0 else 0
+
+    def _apply_brightness(self, source_value: int) -> int:
+        match self.instruction:
+            case Instruction.TURN_ON:
+                return source_value + 1
+            case Instruction.TURN_OFF:
+                return max(source_value - 1, 0)
+            case Instruction.TOGGLE:
+                return source_value + 2
+
+
+class LightsGrid:
+    grid: list[list[int]]
+    size: int
+    brightness: bool
+
+    def __init__(self, size: int, brightness: bool = False):
+        self.size = size
+        self.grid = [[0 for j in range(self.size)] for i in range(self.size)]
+        self.brightness = brightness
+
+    def apply_instruction_line(self, line: InstructionLine) -> None:
+        for i in range(line.start_pos[0], line.end_pos[0] + 1):
+            for j in range(line.start_pos[1], line.end_pos[1] + 1):
+                self.grid[i][j] = line.apply(
+                    source_value=self.grid[i][j], brightness=self.brightness
+                )
+
+    @property
+    def lights_on(self) -> int:
+        return sum(self.grid[i][j] for i in range(self.size) for j in range(self.size))
