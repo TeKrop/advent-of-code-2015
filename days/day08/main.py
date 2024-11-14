51e4e03456ebef8db448f51c62f37c6dee242b8b
TeKrop <tekrop@gmail.com>
2024-11-15 00:06:20 +0100

    updated codebase from template and created day08

---
+++ days/day08/main.py
@@ -0,0 +1,17 @@
+from scripts.utils import AbstractPuzzleSolver
+
+
+class PuzzleSolver(AbstractPuzzleSolver):
+    ###########################
+    # DAY 08 - First Part
+    ###########################
+
+    def _solve_first_part(self) -> int:
+        return None
+
+    ###########################
+    # DAY 08 - Second Part
+    ###########################
+
+    def _solve_second_part(self) -> int:
+        return None
