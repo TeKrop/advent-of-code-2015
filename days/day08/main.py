079017478b0b23ae1243c5fc0f7e5fa3c952bfc1
TeKrop <tekrop@gmail.com>
2024-11-17 13:43:26 +0100

    day 8

--- days/day08/main.py
+++ days/day08/main.py
@@ -1,3 +1,6 @@
+import re
+from functools import cached_property
+
 from scripts.utils import AbstractPuzzleSolver
 
 
@@ -7,11 +10,42 @@ class PuzzleSolver(AbstractPuzzleSolver):
     ###########################
 
     def _solve_first_part(self) -> int:
-        return None
+        return sum(
+            string.nb_char_of_code - string.nb_char_in_memory
+            for line in self.lines
+            if (string := SantaString(line))
+        )
 
     ###########################
     # DAY 08 - Second Part
     ###########################
 
     def _solve_second_part(self) -> int:
-        return None
+        return sum(
+            string.nb_char_in_encoded_string - string.nb_char_of_code
+            for line in self.lines
+            if (string := SantaString(line))
+        )
+
+
+class SantaString:
+    def __init__(self, line: str):
+        self.line = line
+
+    @cached_property
+    def nb_char_of_code(self) -> int:
+        return len(self.line)
+
+    @cached_property
+    def nb_char_in_memory(self) -> int:
+        # First replace \xNN with one character
+        computed_str = re.sub(r"\\x[a-f0-9]{2}", "'", self.line[1:-1])
+        # Then replace the backslashes
+        computed_str = computed_str.replace("\\\\", "\\").replace('\\"', '"')
+        return len(computed_str)
+
+    @cached_property
+    def nb_char_in_encoded_string(self) -> int:
+        nb_quotes = self.line.count('"')
+        nb_backslashes = self.line.count("\\")
+        return self.nb_char_of_code + nb_quotes + nb_backslashes + 2
